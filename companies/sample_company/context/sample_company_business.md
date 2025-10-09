3e824157a2e47fe2f389aab013934d45d96b8b57
Fernando Libedinsky <flibedinsky@maxxa.cl>
2025-10-09 18:42:55 -0300

    optimizaciones para release en maxxa

--- companies/sample_company/context/sample_company_business.md
+++ companies/sample_company/context/sample_company_business.md
@@ -6,11 +6,11 @@
 Su propósito principal es ilustrar cómo un asistente de IA puede conectarse a una base de datos externa 
 para responder preguntas de inteligencia de negocio (Business Intelligence) y realizar análisis de datos en tiempo real.
 
-Se utiliza la base de datos northwind como ejemplo de base de datos transaccional.
-
 ## 2. Modelo de Negocio
 
 El modelo de negocio de Sample Company es directo y se centra en la venta de productos.
+Se dispone de información de sus clientes, proveedores, ordenes de ventas.
+Además de empleados,  sus territorios y paises.
 
 ## 3. Fuentes de Datos
 
@@ -23,20 +23,3 @@ Para Sample Company, el asistente de IA actúa como un **Analista de Datos o de 
 Su función principal es entender preguntas complejas en lenguaje natural, traducirlas a consultas SQL, 
 ejecutar esas consultas en la base de datos de la compañía y presentar los resultados de forma clara y concisa.
 
-### Ejemplos de Consultas que el Asistente puede Resolver:
-
-- **Análisis de Ventas:**
-  - *"¿Cuál fue el total de ventas del mes pasado?"*
-  - *"Muéstrame las ventas totales por mes durante el último año."*
-
-- **Análisis de Productos:**
-  - *"¿Cuál es el producto más vendido de todos los tiempos?"*
-  - *"Lista los 10 productos con menor stock vendido."*
-
-- **Análisis de Clientes:**
-  - *"¿Quiénes son nuestros 5 clientes más valiosos (los que más han gastado)?"*
-  - *"Muéstrame el historial de compras del cliente 'Juan Pérez'."*
-
-- **Consultas Combinadas:**
-  - *"¿Qué producto compró más veces el cliente con el email 'ana.lopez@email.com'?"*
-  - *"En la última semana, ¿cuál fue el día con más órdenes registradas?"*
--- src/iatoolkit/__init__.py
+++ src/iatoolkit/__init__.py
@@ -4,37 +4,21 @@ IAToolkit Package
 
 # Expose main classes and functions at the top level of the package
 
-# Assuming 'toolkit.py' contains the IAToolkit class
-from .iatoolkit import IAToolkit, create_app
-from .iatoolkit import current_iatoolkit
+# main IAToolkit class
+from .iatoolkit import IAToolkit, current_iatoolkit, create_app
 
-# Assuming 'app_factory.py' contains create_app and register_company
+# for registering the client companies
 from .company_registry import register_company
-
-# Assuming 'base_company.py' contains BaseCompany
 from .base_company import BaseCompany
+from iatoolkit.repositories.database_manager import DatabaseManager
 
 # --- Services ---
-# Assuming they are in a 'services' sub-package
 from iatoolkit.services.sql_service import SqlService
-from iatoolkit.services.excel_service import ExcelService
-from iatoolkit.services.dispatcher_service import Dispatcher
 from iatoolkit.services.document_service import DocumentService
 from iatoolkit.services.search_service import SearchService
 from iatoolkit.services.load_documents_service import LoadDocumentsService
-from iatoolkit.repositories.profile_repo import ProfileRepo
-from iatoolkit.repositories.llm_query_repo import LLMQueryRepo
-
-from iatoolkit.services.query_service import QueryService
-from iatoolkit.services.prompt_manager_service import PromptService
-from iatoolkit.repositories.database_manager import DatabaseManager
-
+from iatoolkit.services.excel_service import ExcelService
 from iatoolkit.infra.call_service import CallServiceClient
-from iatoolkit.common.util import Utility
-
-
-from iatoolkit.repositories.models import Base, Company, Function, TaskType, Prompt, PromptCategory
-
 
 __all__ = [
     'IAToolkit',
@@ -42,23 +26,11 @@ __all__ = [
     'current_iatoolkit',
     'register_company',
     'BaseCompany',
+    'DatabaseManager',
     'SqlService',
     'ExcelService',
-    'Dispatcher',
     'DocumentService',
     'SearchService',
-    'QueryService',
     'LoadDocumentsService',
-    'ProfileRepo',
-    'LLMQueryRepo',
-    'PromptService',
-    'DatabaseManager',
     'CallServiceClient',
-    'Utility',
-    'Company',
-    'Function',
-    'TaskType',
-    'Base',
-    'Prompt',
-    'PromptCategory'
 ]
