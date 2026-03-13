35b982fc9bb0d28e894966d6c4ee2141bf859d23
Fernando Libedinsky <flibedinsky@maxxa.cl>
2026-03-13 10:39:27 +0100

    bootstrap fixes

--- docs/migrations/2026-02-phase2-remove-document-content.sql
+++
@@ -1,5 +0,0 @@
--- Phase 2 migration: remove deprecated iat_documents.content column
--- Run this on PostgreSQL before deploying the phase 2 code.
-
-ALTER TABLE iat_documents
-DROP COLUMN IF EXISTS content;
